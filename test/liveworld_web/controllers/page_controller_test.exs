12719909bd78e3666290c35a0db990cbe1b2937c
sazzy-josh <idahosajoshua61@gmail.com>
2023-02-03 08:32:52 +0100

    initials

---
+++ test/liveworld_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule LiveworldWeb.PageControllerTest do
+  use LiveworldWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
